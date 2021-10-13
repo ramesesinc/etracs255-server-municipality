979420779c2b6e3641b21c65df6c8eb990cd41a5
worgflores <worgflores@gmail.com>
2021-10-13 15:00:17 +0800

    updated initial databases

--- _res/training/_initial/training_etracs255.sql
+++ _res/training/_initial/training_etracs255.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `training_etracs255` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `training_etracs255`;
+
 --
 -- Table structure for table `account`
 --
